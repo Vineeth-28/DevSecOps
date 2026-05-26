eefe69434fe7f6701358ca9268a1053367392927
Vineeth-28 <vineetprakash918@gmail.com>
2026-05-26 06:26:15 -0700

    eslint config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.js
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
